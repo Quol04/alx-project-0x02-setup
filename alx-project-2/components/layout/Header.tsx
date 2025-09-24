e263e4785af359115a6ee8dfe72ccb7d997d0bb6
quol <kuolamol77@gmail.com>
2025-09-24 10:16:49 +0300

    added the home and about routes

--- alx-project-2/components/layout/Header.tsx
+++ alx-project-2/components/layout/Header.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Header: React.FC = () => {
   return (
     <div>
-        <h1> This is the Header</h1>
+        <h1> Logo</h1>
+        <nav>
+            <a href="/home">Home</a>
+            <a href="/about">About</a>
+        </nav>
     </div>
   )
 }
--- alx-project-2/pages/index.tsx
+++ alx-project-2/pages/index.tsx
@@ -1,7 +1,10 @@
+import Header from "@/components/layout/Header";
+import React from "react";
 
 export default function Home() {
   return (
     <div>
+      <Header/>
       <h1>Welcome to the Next.js App!</h1>
     </div>
   )
