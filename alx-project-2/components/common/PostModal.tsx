c2dc51f81f0765944d707ae28472a1e06b708b72
quol <kuolamol77@gmail.com>
2025-09-25 11:09:40 +0300

    added a new folder

--- alx-project-2/components/common/PostModal.tsx
+++ alx-project-2/components/common/PostModal.tsx
@@ -2,15 +2,33 @@ import { CardProps } from '@/interfaces'
 import React from 'react'
 
 const PostModal : React.FC< CardProps> = ({title, content}) => {
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  }
   return (
     <div>
-        <form action="">
-            <h2>{title}</h2>
-            <p>{content}</p>
-            <input type="text" placeholder='Title' />
-            <textarea placeholder='Content'></textarea>
-            <button type='submit'>Submit</button>
-        </form>
+       <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input 
+          type="text" 
+          id="title" 
+          value={title}
+          placeholder='Enter the Title' />
+        </div>
+        <div>
+          <label htmlFor="content">Content</label>
+          <textarea 
+            value={content}
+            id="content" 
+            placeholder='Enter the Content'></textarea>
+        </div>
+        <div>
+          <button type="button">Cancel</button>
+          <button type="submit">Submit</button>
+        </div>
+       </form>
     </div>
   )
 }
